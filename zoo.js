6038aabd08126d1600d2ee71a29dfa777c36007c
Nicholas Gallardo <nic.gallardo@gmail.com>
2015-08-04 07:41:23 -0600

    first repo

---
+++ zoo.js
@@ -0,0 +1,21 @@
+ var zoo ={ 
+	lions: 0,
+	bears: 1,
+	liger: 200,
+	lionFood: 100,
+	lionSum: function(amt){
+ 		this.lions += amt
+ 		},
+ 	bearSum: function(amt){
+ 		this.bears += amt
+ 		},
+ 	ligerSum: function(amt){
+ 		this.liger += amt
+ 		},
+ 	lionFoodTracker: function(amt){
+ 		this.lionFood += amt
+ 		},
+ 	}
+
+ 	}	
+ 
---
+++ zooConstructor.js
@@ -0,0 +1,7 @@
+function ZooTracker(animalType, foodAmt,){
+	this.animal = animalType;
+	this.food = foodAmt;
+	this.foodTrack = function(changeAmt){
+		this.food = cxhangeAmt;
+	};
+}
